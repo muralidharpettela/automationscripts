3535b4c48ee8c403e62c56f231dd1be8b57b0b0f
muralidhar <muralidharpettela@gmail.com>
2022-03-21 20:40:09 +0100

    facebook

--- facebook_posts/fb_bot.py
+++ facebook_posts/fb_bot.py
@@ -1,11 +1,12 @@
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
 import time
 from selenium.webdriver.common.keys import Keys
 import json
 import os
+from selenium.webdriver.common.by import By
+import autoit
 
-LOGIN_URL = 'https://www.facebook.com/login.php'
+LOGIN_URL = 'https://www.facebook.com/'
 GROUP_URL = 'https://www.facebook.com/groups/'
 
 options = webdriver.ChromeOptions()
@@ -15,7 +16,7 @@ options.add_argument("--disable-notifications")
 # ^disale usb device error
 options.add_experimental_option('excludeSwitches', ['enable-logging'])
 
-driver = webdriver.Chrome('./chromedriver.exe', options=options)
+driver = webdriver.Chrome('chromedriver.exe', options=options)
 
 
 class FacebookBot():
@@ -25,10 +26,10 @@ class FacebookBot():
         self.mode = mode
         self.textContents = text
         self.groupdata = groups
-        self.fb_posting = ["//div[@class='m9osqain a5q79mjw jm1wdb64 k4urcfbm']",
+        self.fb_posting = ["/html/body/div[1]/div/div[1]/div/div[3]/div/div/div[1]/div[1]/div[2]/div/div/div[4]/div/div/div/div/div[1]/div[1]/div/div/div/div[1]/div/div[1]/span",
                            "/html/body/div[1]/div/div[1]/div/div[4]/div/div/div[1]/div/div[2]/div/div/div/div/div[1]/form/div/div[1]/div/div/div[1]/div/div[2]/div[1]/div[1]/div[1]/div/div/div/div/div[2]/div/div/div/div",
-                           "/html/body/div[1]/div/div[1]/div/div[4]/div/div/div[1]/div/div[2]/div/div/div/div/div[1]/form/div/div[1]/div/div/div[1]/div/div[3]/div[2]/div/div",
-                           "/html/body/div[1]/div/div[1]/div/div[4]/div/div/div[1]/div/div[2]/div/div/div/div/div[1]/form/div/div[1]/div/div/div[1]/div/div[3]/div[1]/div[2]/div[3]/input"]
+                           "/html/body/div[1]/div/div[1]/div/div[4]/div/div/div[1]/div/div[2]/div/div/div/div/div[1]/form/div/div[1]/div/div/div[1]/div/div[3]/div[2]/div[1]/div/div",
+                           "/html/body/div[1]/div/div[1]/div/div[4]/div/div/div[1]/div/div[2]/div/div/div/div/div[1]/form/div/div[1]/div/div/div[1]/div/div[2]/div[1]/div[2]/div/div[1]/div/div[1]/div/div[1]/div/div/div/div[1]/div/i"]
 
     # ^Group ids
     def ids(self):
@@ -46,11 +47,14 @@ class FacebookBot():
 
         driver.get(LOGIN_URL)
         time.sleep(2)  # Wait for some time to load
+        accept_c = driver.find_element(By.XPATH, '/html/body/div[3]/div[2]/div/div/div/div/div[3]/button[2]')
+        accept_c.click()
         email_element = driver.find_element_by_id('email')
         email_element.send_keys(self.email)  # Give email as i/p
         password_element = driver.find_element_by_id('pass')
         password_element.send_keys(self.password)  # Give password as i/p
-        login_button = driver.find_element_by_id('loginbutton')
+        login_button = driver.find_element_by_xpath(
+            '/html/body/div[1]/div[2]/div[1]/div/div/div/div[2]/div/div[1]/form/div[2]/button')
         login_button.click()  # Login click
         time.sleep(2)  # Wait for 2 seconds for the page to show up
 
@@ -73,7 +77,8 @@ class FacebookBot():
 
                 if (self.mode):
                     try:
-                        x = driver.find_element_by_xpath(self.fb_posting[3])
+                        driver.find_element_by_xpath("/html/body/div[1]/div/div[1]/div/div[4]/div/div/div[1]/div/div[2]/div/div/div/div/div[1]/form/div/div[1]/div/div/div[1]/div/div[3]/div[1]/div[2]/div[1]/div/span/div/div/div[1]/div/div/div[1]/i").click()
+                        x = driver.find_element_by_xpath(self.fb_posting[3]).click()
                         time.sleep(2)
 
                         # ^get current directory
@@ -91,7 +96,10 @@ class FacebookBot():
                         # ^all paths
 
                         for p in os.listdir():
-                            x.send_keys(cwd + path + "\{0}".format(p))
+                            autoit.control_focus("Open", "Edit1")
+                            autoit.control_set_text("Open", "Edit1", cwd + path + "\{0}".format(p))
+                            autoit.control_click("Open", "Button1")
+                            #x.send_keys(cwd + path + "\{0}".format(p))
 
                         # ^clear dir
                         os.chdir(cwd)
