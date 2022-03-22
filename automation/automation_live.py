01028d6c623fe7bc1fb1d34d47c44b6b0da1531f
muralidharpettela <muralidharpettela@gmail.com>
2022-03-22 22:15:00 +0100

    Updated paths

--- automation/automation_live.py
+++ automation/automation_live.py
@@ -21,7 +21,7 @@ class Handler_Class(object):
                         saved_file_location = os.path.join(outputDir, attachment.FileName)
                         attachment.SaveAsFile(saved_file_location)
                         file_path = Path.cwd()
-                        products_json_path = os.path.join(file_path, "../update_stock/products.json")
+                        products_json_path = os.path.join(file_path, "update_stock/products.json")
                         live_products_update = LiveUpdateProducts(saved_file_location, products_json_path)
                         live_products_update.process()
                         print(f"attachment saved")
